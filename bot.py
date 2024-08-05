9edb7023e7eb40eda9701c9cbf26c375f77d15a2
Crzidea <i@crzidea.com>
2024-08-05 17:56:15 +0800

    Fix a lot of bugs

--- bot.py
+++ bot.py
@@ -1,13 +1,31 @@
+import asyncio
 import discord
 import io
 import logging
 import os
 import pydub
+import struct
 
 logging.basicConfig(level=logging.INFO)
 
 # Read token from environment variable
 token = os.getenv("DISCORD_TOKEN")
+if token is None:
+    token = userdata.get("DISCORD_TOKEN")
+
+
+# discord.VoiceClient.supported_modes = "xsalsa20_poly1305"
+# Override code to fix protocol
+def strip_header_ext(self, data):
+    if len(data) == 0:
+        return data
+    if data[0] == 0xBE and data[1] == 0xDE and len(data) > 4:
+        _, length = struct.unpack_from(">HH", data)
+        offset = 4 + length * 4
+        data = data[offset:]
+    return data
+
+discord.VoiceClient.strip_header_ext = strip_header_ext 
 
 # Create intents object
 intents = discord.Intents.none()
@@ -18,8 +36,6 @@ intents.guild_messages = True
 
 # Create the bot instance
 bot = discord.Bot(intents=intents, command_prefix="/")
-
-connections = {}
 
 
 @bot.command(name="join", description="Join a voice channel")
@@ -42,16 +58,36 @@ async def join_command(
         await interaction.edit_original_response(
             content=f"Already connected to a voice channel, leaving and joining {channel.name}!"
         )
-        await ctx.guild.voice_client.disconnect()
-    await channel.connect(timeout=3)
+        await ctx.guild.voice_client.disconnect(force=True)
+
+    # Retry if timeout exceed
+    async def retry(
+        channel: discord.VoiceChannel, interaction, maxRetryTimes, retryTimes
+    ):
+        try:
+            await asyncio.wait_for(channel.connect(), 3)
+        except asyncio.TimeoutError:
+            await channel.guild.voice_client.disconnect(force=True)
+            if retryTimes < maxRetryTimes:
+                await interaction.edit_original_response(
+                    content=f"Failed to join {channel.name} {retryTimes} times! Timeout exceeded."
+                )
+                await retry(channel, interaction, maxRetryTimes, retryTimes + 1)
+            else:
+                await interaction.edit_original_response(
+                    content=f"Failed to join {channel.name}! Max retry times exceeded."
+                )
+                raise
+
+    await retry(channel, interaction, 3, 1)
+    # await channel.connect(timeout=3)
+    await ctx.guild.change_voice_state(channel=channel, self_mute=True)
+
     # Start listening for audio
     await interaction.edit_original_response(content=f"Joined {channel.name}!")
 
     # https://guide.pycord.dev/voice/receiving
-    vc = ctx.guild.voice_client
-    connections.update(
-        {ctx.guild.id: vc}
-    )  # Updating the cache with the guild and channel.
+    vc = channel.guild.voice_client
 
     vc.start_recording(
         MySink(),  # The sink type to use.
@@ -60,11 +96,13 @@ async def join_command(
     )
     await interaction.edit_original_response(content="Started recording!")
 
+
 # Implenment a sink that convert audio data into AudioSegment
 class MySink(discord.sinks.Sink):
+    audio_segments = {}
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.audio_segments = {}
 
     @discord.sinks.Filters.container
     def write(self, data, user):
@@ -78,44 +116,30 @@ class MySink(discord.sinks.Sink):
             channels=2,
             frame_format="s16le",
         )
-        if user not in self.audio_segments:
-            self.audio_segments.update({user: []})
-        # self.audio_segments[user].append(audio_segment)
+        last_chunk = self.audio_segments.get(user)
+        # TODO
+        last_chunk = audio_segment
+        # Process the audio segment
+        self.audio_segments.update({user: last_chunk})
+        # process_audio_segment(user, None)
+        # process_audio_segment(audio_segment, last_chunk)
 
     def cleanup(self):
         self.finished = True
-        for user, segments in self.audio_segments.items():
-            # Concatenate the audio segments
-            audio_segment = pydub.AudioSegment.empty()
-            for segment in segments:
-                audio_segment += segment
-            # Save the audio segment to a file
-            audio_segment.export(f"{user}.wav", format="wav")
+        self.audio_segments.clear()
+        print("Cleanup called")
 
 
 # Define a callback function to be called when recording is finished
-async def once_done(
-    sink: discord.sinks, channel: discord.TextChannel, *args
-):  # Our voice client already passes these in.
+async def once_done(sink: discord.sinks, channel: discord.TextChannel, *args):
     pass
-    # recorded_users = [  # A list of recorded users
-    #     f"<@{user_id}>" for user_id, audio in sink.audio_data.items()
-    # ]
-    # await sink.vc.disconnect()  # Disconnect from the voice channel.
-    # files = [
-    #     discord.File(audio.file, f"{user_id}.{sink.encoding}")
-    #     for user_id, audio in sink.audio_data.items()
-    # ]  # List down the files.
-    # await channel.send(
-    #     f"finished recording audio for: {', '.join(recorded_users)}.", files=files
-    # )  # Send a message with the accumulated files.
 
 
 @bot.command(name="leave", description="Leave the current voice channel")
 async def leave_command(ctx: discord.ApplicationContext):
     voice_client = ctx.guild.voice_client
     if voice_client:
-        await voice_client.disconnect()
+        await voice_client.disconnect(force=True)
         await ctx.response.send_message("Left the voice channel.")
     else:
         await ctx.response.send_message("Not connected to any voice channel.")
@@ -132,4 +156,16 @@ async def on_message(message):
     print(f"Received message from {message.author}: {message.content}")
 
 
-bot.run(token)
+# bot.run(token)
+
+
+def process_audio_segment():
+    pass
+
+
+try:
+    loop = asyncio.get_running_loop()
+    loop.create_task(bot.run(token))
+except RuntimeError:
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(bot.run(token))
